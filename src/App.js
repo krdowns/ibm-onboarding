241a01073bab4f125fff82352414c82a79b23e46
Kenny Downs <kdowns@repairpal.com>
2019-12-07 20:15:46 -0800

    created background placeholder, added image inside modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+
+import background from "./background.jpg";
+import scene from "./Scene_3_1000.png";
+import { Modal } from "carbon-components-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="modal-container">
+        <Modal
+          className="onboarding-modal"
+          focusTrap={false}
+          hasScrollingContent
+          iconDescription="Close the modal"
+          aria-label="Label"
+          modalAriaLabel="A label to be read by screen readers on the modal root node"
+          onBlur={function noRefCheck() {}}
+          onClick={function noRefCheck() {}}
+          onFocus={function noRefCheck() {}}
+          onKeyDown={function noRefCheck() {}}
+          onRequestClose={function noRefCheck() {}}
+          onRequestSubmit={function noRefCheck() {}}
+          onSecondarySubmit={function noRefCheck() {}}
+          open
+          passiveModal={false}
+          primaryButtonDisabled={false}
+          primaryButtonText="Next"
+          secondaryButtonText="Back"
+          selectorPrimaryFocus="[data-modal-primary-focus]"
         >
-          Learn React
-        </a>
-      </header>
+          <React.Fragment key=".1">
+            <div className="scene-container">
+              <img className="scene" src={scene} alt="scene" />
+            </div>
+          </React.Fragment>
+          <div>
+            <p>
+              Esse quis fugiat consequat ut cillum sit cillum enim in commodo
+              cupidatat. Incididunt minim commodo adipisicing irure
+              reprehenderit ut sit aliquip ullamco mollit aute ullamco
+              excepteur. Occaecat velit anim reprehenderit excepteur ea veniam
+              consequat consectetur excepteur aute voluptate minim proident
+              velit. Sint magna dolor veniam voluptate est pariatur deserunt
+              excepteur sunt sit qui. Veniam aliquip mollit mollit ut ullamco.
+            </p>
+          </div>
+        </Modal>
+      </div>
+      <img className="background" src={background} alt="background" />
     </div>
   );
 }
